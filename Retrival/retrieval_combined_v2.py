cb412a17f2a2f7b9836a763bfe5ad2cc8f6e3b3f
Harsh Chinchakar <harshchinchakar33@gmail.com>
2025-10-29 12:17:34 +0530

    Update retrieval_combined_v2.py

--- Retrival/retrieval_combined_v2.py
+++ Retrival/retrieval_combined_v2.py
@@ -46,8 +46,11 @@ except Exception as e:
 logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s")
 log = logging.getLogger("retrieval")
 
+
+BASE_DIR = Path(__file__).resolve().parent
 # ---------------- Config & helpers ----------------
-DEFAULT_INDEX_DIR = Path("Embedding/Data_Base")
+# DEFAULT_INDEX_DIR = Path("Embedding/Data_Base")
+DEFAULT_INDEX_DIR = BASE_DIR / "Embedding" / "Data_Base"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 CROSS_ENCODER_MODEL = "cross-encoder/ms-marco-MiniLM-L-6-v2"  # optional reranker
 SNIPPET_LEN = 350
